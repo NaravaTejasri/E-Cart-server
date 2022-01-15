ba53eb8cfe15ecd28b51ea532469d448a99f5f00
NaravaTejasri <tejasrisony17@gmail.com>
2022-01-15 11:48:30 +0100

    committing auth and config

--- migrations/20220115102225-create-category.js
+++ migrations/20220115102225-create-category.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("category", {
+    await queryInterface.createTable("categories", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -27,6 +27,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("category");
+    await queryInterface.dropTable("categories");
   },
 };
